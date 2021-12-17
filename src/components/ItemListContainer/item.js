32cb1476f7aa5bcf7e70247136662d55627f19c4
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-12-16 23:50:17 -0300

    actualizando card

--- src/components/ItemListContainer/item.js
+++ src/components/ItemListContainer/item.js
@@ -7,15 +7,19 @@
     }
 return (
     
-        <div className="row">
-            <div className="card container card-columns row  col-10" style={{width:'18rem'}} onClick={handleClick}>
+        <div className="container ">
+        <div className="row mx-auto">
+            <div className="col-12">
+            <div className="card  col-md-6 col-lg-4 mb-2 mb-md-0 text-center" style={{width:'18rem'}} onClick={handleClick}>
                 <img src={products.img} alt={products.name} className="card-img-top"/>
                 <div className="card-body">
                 <h4 className="card-title">Nombre:{products.name}</h4>
-                <p className="card-text">Categoria:{products.catergory}</p>
+                <p className="card-text">Categoria:{products.category}</p>
                 <p className="card-text">Precio:{products.price}</p> 
                 <button id='${producto.id}' className="btn btn-dark btn-compra">COMPRAR</button>
                 </div>
+            </div>
+            </div>
             </div>
         </div>
 
